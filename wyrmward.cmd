5359c48fb5ae71d487865a135ed447c5a436c852
Johan Berntsson <johan.mayumi@gmail.com>
2026-07-26 15:52:41 +0900

    Improved texts

--- wyrmward.cmd
+++ wyrmward.cmd
@@ -4,6 +4,11 @@ restart
 yes
 random seed 1
 take scroll
+listen
+smell
+e
+u
+d
 x scroll
 i
 x bag
@@ -21,6 +26,11 @@ follow path
 enter forest
 take forest
 enter hut
+listen
+smell
+u
+d
+w
 x bench
 x window
 x vessels
@@ -33,63 +43,264 @@ x istar
 read folio
 give scroll to istar
 x scroll
-copy animation spell
+cast copy on animation spell
 n
 copy teleportation spell
 follow istar
-teleport
+n
+n
+cast teleport
+listen
+smell
+x steppe
+x archway
+x archways
+x compass
+x yellow cloud
 e
+listen
+smell
+x beggar
+x cornfields
+x village
+x tree
+climb tree
+talk to beggar
+ask beggar about stones
+give spellbook to beggar
+search beggar
+attack beggar
+n
+w
+u
+d
 e
+listen
+smell
+x house
+x road
+x houses
+x village
+x hawker
+n
+u
 e
+listen
+smell
+x coin
 take coin
+i
+n
+e
+u
+d
+x village
+x houses
+x corn
 w
 z
 z
 z
 point at left cup
+x stone
+take stone
+buy stone
+ask hawker about stone
+hawker, give me stone
+save
+
+w
+give coin to beggar
+e
 give coin to hawker
-animate bench
+z
+z
+z
+x middle cup
+point at left cup
+give coin to hawker
+z
+z
+z
+point at right cup
+give coin to hawker
+restore
+
+give coin to hawker
+cast animate on bench
+i
 w
 give coin to beggar
 wear amulet
-teleport
+i
+x amulet
+cast teleport
+listen
+smell
 n
-e
-e
+x mature forest
+x mountain
+x path
+x tree
 n
+listen
+smell
+u
+d
+e
+x dark forest
+x mountain
+x path
+x tree
+climb tree
+n
+listen
+smell
+u
+d
+e
+listen
+smell
+x great mountain
+x forest
+x path
+climb mountain
+x cave mouth
+s
+u
+d
+n
+listen
+smell
+x chamber
+x light
+x shadow
+x tunnel
+x green stone
 take stone
 illuminate it
+e
+u
+d
 n
+listen
+smell
+x scroll
+x cavity
+x feast hall
+x storeroom
+x slope
+e
+u
+x crates
+x packages
+search crate
+take package
 take scroll
 read it
 n
-animate left
-animate right
-freeze mud
-z
-heal dragon
-ask dragon about magic
-teleport
-w
+listen
+smell
+x water
+x floor
+x mud
 e
+x statues
+x left
+x right
+left, hello
+talk to right
+ask right about green stone
+cast animate on left
+cast animate on right
+ask right about green stone
+cast freeze on mud
+z
+x ash
+smell
+listen
+x sulfur
+x incline
+x slope
+z
+x dragon
+e
+cast heal on dragon
+ask dragon about me
+show spellbook to dragon
+ask dragon about magic
+x spellbook
+read spellbook
+cast teleport
+w
+smell
+listen
+x temple
+x sea
+x mountain
+w
+u
+d
+e
+e
+u
+d
+smell
+listen
+x vaults
+x leaded windows
+x temple
+x statue
+talk to statue
+take statue
+kill statue
 pray
+x me
 w
 s
+u
+d
+e
+x waves
+smell
+listen
 swim
+smell
+listen
+x sand
+x shells
+x blue stone
 take stone
 u
+cast teleport
 n
 e
 pray
-teleport
+cast teleport
 s
 s
 put green stone in box
 put blue stone in box
 put red stone in box
+
+save
+
+y
+x force field
+x demon
+x altar
+x pillars
+x pedestals
+
+
+restore
+
+n
+smell
+listen
 touch field with amulet
-kill demon
-resurrect mentor
+cast death spell on  demon
+cast resurrect spell on istar
 z
 z
 z
